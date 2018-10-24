5b01e9b7bdf6d4847c4f25fe5a9d90cc9a83436b
cbhcbhcbh <15862528607@163.com>
2018-10-24 12:49:13 +0800

    Madelung_constant

--- Madelung_constant/Madelung_constant.py
+++ Madelung_constant/Madelung_constant.py
@@ -7,14 +7,14 @@ n = 100
 
 def TrueorFalse(name):
 	if name == 'NaCl':
-		Madelung_NaCl(name, n)
+		Madelung_NaCl( n)
 	elif name == 'CsCl':
-		Madelung_CsCl(name, n)
+		Madelung_CsCl(n)
 	else:
 		print("╮(╯3╰)╭")
 
 
-def Madelung_NaCl(name, n):
+def Madelung_NaCl(n):
 	result = 0
 	for n1 in range(-n, n+1):
 		for n2 in range(-n, n+1):
@@ -24,34 +24,53 @@ def Madelung_NaCl(name, n):
 	print(result)
 
 
-# def Madelung_CsCl(name, n):
-
-"""
-def CsCl8(N):
-    negative, positive = 0.0, 0.0
+def Madelung_CsCl(n):
+	"""
+	function CsCl48(N::Int64)
     N = N + (N & 0x1)
-    N2 = N**2
-    for n1 in range(1, N + 1, 2):
-        n12 = n1**2
-        for n2 in range(1, N + 1, 2):
-            AB = n12 + n2**2
-            for n3 in range(1, N + 1, 2):
-                negative -= 1 / math.sqrt(AB + n3**2)
-    for n1 in range(2, N, 2):
-        n12 = n1**2
-        positive += 0.75 / n1                    # 3条坐标轴
-        positive += 0.75 / math.sqrt(n12 + 2*N2)  # 与坐标轴平行的3条棱
-        positive += 1.5 / math.sqrt(n12 + N2)   # 与坐标轴垂直的6条棱
-        for n2 in range(2, N, 2):
-            AB = n12 + n2**2
-            positive += 1.5 / math.sqrt(AB)      # 3个坐标面
-            positive += 1.5 / math.sqrt(AB + N2)  # 与坐标轴垂直的3个面
-            for n3 in range(2, N, 2):
-                positive += 1 / math.sqrt(AB + n3**2)
-    positive += (3 + 3 / math.sqrt(2) + 1 / math.sqrt(3)) / (8 * N)
-    return -8 * (positive + negative)
-"""
-
-
+    negative, positive, N² = 0.0, 0.0, N^2
+    for n₁ ∈ 1:2:N
+        # 体对角线: 1/6 × 1/√3 = √3/18
+        negative -= √3/18 / n₁
+        n₁² = n₁^2
+        for n₂ ∈ n₁+2:2:N
+            n₂² = n₂^2
+            AB = n₁² + n₂²
+            # 在三棱锥面上的原子, 1/2
+            negative -= 1/2 / √(AB + n₁²)  # x = y
+            negative -= 1/2 / √(AB + n₂²)  # y = z
+            # 三棱锥内部的原子
+            for n₃ ∈ n₂+2:2:N
+                negative -= 1 / √(AB + n₃^2)
+            end
+        end
+    end
+    for n₁ ∈ 2:2:N-2
+        # 坐标轴上: 1/8
+        # 面对角线: 1/4 × 1/√2 = √2/8
+        # 体对角线: 1/6 × 1/√3 = √3/18
+        n₁² = n₁^2
+        positive += (1/8 + √2/8 + √3/18) / n₁
+        positive += 1/8 / √(2N² + n₁²)  # x = N, y = N
+        positive += 1/4 / √(N² + 2n₁²)  # x = N, y = z
+        positive += 1/4 / √(N² + n₁²)   # x = N, z = 0
+        for n₂ ∈ n₁+2:2:N-2
+            n₂² = n₂^2
+            AB = n₁² + n₂²
+            # 在三棱锥面上的原子, 1/2
+            positive += 1/2 / √(AB)        # z = 0
+            positive += 1/2 / √(AB + N²)   # x = N
+            positive += 1/2 / √(AB + n₁²)  # x = y
+            positive += 1/2 / √(AB + n₂²)  # y = z
+            # 三棱锥内部的原子
+            for n₃ ∈ n₂+2:2:N-2
+                positive += 1 / √(AB + n₃^2)
+            end
+        end
+    end
+    positive += (1 + √2/2 + √3/9)/16N
+    return -48(negative + positive)
+end
+	"""
 
 TrueorFalse(name)
