20bcdfe877e9fab2f9d11ba0eee0400c1ebc7595
cbhcbhcbh <15862528607@163.com>
2018-10-24 19:33:02 +0800

    Madelung_constant

--- Madelung_constant/Madelung_constant.py
+++ Madelung_constant/Madelung_constant.py
@@ -2,7 +2,7 @@ import math # 导入sqrt
 
 
 name = str(input("请输入离子晶体的名称："))
-n = 100
+n = int(input("请输入晶胞个数："))
 
 
 def TrueorFalse(name):
@@ -24,57 +24,45 @@ def Madelung_NaCl(n):
 	print(result)
 
 
-"""
-    # 与中心离子相同
-    for n₁ ∈ 2:2:N-2
-        # 坐标轴上: 1/8
-        # 面对角线: 1/4 × 1/√2 = √2/8
-        # 体对角线: 1/6 × 1/√3 = √3/18
-        n₁² = n₁^2
-        positive += (1/8 + √2/8 + √3/18) / n₁
-        positive += 1/8 / √(2N² + n₁²)  # x = N, y = N
-        positive += 1/4 / √(N² + 2n₁²)  # x = N, y = z
-        positive += 1/4 / √(N² + n₁²)   # x = N, z = 0
-        for n₂ ∈ n₁+2:2:N-2
-            n₂² = n₂^2
-            AB = n₁² + n₂²
-            # 在三棱锥面上的原子, 1/2
-            positive += 1/2 / √(AB)        # z = 0
-            positive += 1/2 / √(AB + N²)   # x = N
-            positive += 1/2 / √(AB + n₁²)  # x = y
-            positive += 1/2 / √(AB + n₂²)  # y = z
-            # 三棱锥内部的原子
-            for n₃ ∈ n₂+2:2:N-2
-                positive += 1 / √(AB + n₃^2)
-            end
-        end
-    end
-    positive += (1 + √2/2 + √3/9)/16N
-    return -48(negative + positive)
-end
-	"""
 
 def Madelung_CsCl(n):
     negative, positive = 0, 0
+    n = n + (n & 0x1)
 
-    # 与中心离子相同
-    for n1 in range(1, n+1, 2):
-        # 线上的（4种）
-        positive += (1/8) / n1
-        
-        
     # 与中心离子相反
     for n1 in range(1, n, 2):
         # 体对角线
-        negative -= (math.sqrt(3)) / (18 * n1)
+        negative -= (math.sqrt(3)/18) / n1
         # 面上离子
         for n2 in range(n1+2, n, 2):
-            length = n1 ** 2 + n2 ** 2
-            negative -= (1/2) / (math.sqrt(length + n2**2)) + (0.5) / (math.sqrt(length + n1**2))
+            length1 = n1 ** 2 + n2 ** 2
+            negative -= (1/2) / (math.sqrt(length1 + n2**2)) 
+            negative -= (1/2) / (math.sqrt(length1 + n1**2))
             # 内部离子
             for n3 in range(n2+2, n, 2):
-                negative -= 1 / (math.sqrt(length + n3**2))
+                negative -= 1 / (math.sqrt(length1 + n3**2))
 
+    # 与中心离子相同
+    for n1 in range(1, n-2, 2):
+        # 线上的（4种）
+        positive += (1/8 + math.sqrt(2)/8 + math.sqrt(3)/18) / n1
+        positive += 1/8 / (math.sqrt(2 * n**2 + n1**2))
+        positive += 1/4 / (math.sqrt(n**2 + n1**2))
+        positive += 1/4 / (math.sqrt(n**2 + 2 * n1**2))
+        # 面上
+        for n2 in range(n1+2, n-2, 2):
+            length2 = n1**2 + n2**2
+            positive += 1/2 / (math.sqrt(length2))
+            positive += 1/2 / (math.sqrt(length2 + n**2))
+            positive += 1/2 / (math.sqrt(length2 + n1**2))
+            positive += 1/2 / (math.sqrt(length2 + n2**2))
+            #内部
+            for n3 in range(n2+2, n-2, 2):
+                positive += 1 / (math.sqrt(length2 + n3**2))
+    positive += (1 + math.sqrt(2)/2 + math.sqrt(3)/9) / 16*n
+    
+    result = (negative + positive) * (48)
+    print(result)
 
 
 TrueorFalse(name)
