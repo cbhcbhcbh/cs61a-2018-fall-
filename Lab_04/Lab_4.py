4f6d0db39a2f8a447ca29aa6e978139603db1d4a
cbhcbhcbh <15862528607@163.com>
2018-11-01 20:24:56 +0800

    再看一遍

---
+++ Lab_04/Lab_4.py
@@ -0,0 +1,371 @@
+from math import sqrt
+def if_this_not_that(i_list, this):
+    """Define a function which takes a list of integers `i_list` and an integer
+    `this`. For each element in `i_list`, print the element if it is larger
+    than `this`; otherwise, print the word "that".
+
+    >>> original_list = [1, 2, 3, 4, 5]
+    >>> if_this_not_that(original_list, 3)
+    that
+    that
+    that
+    4
+    5
+    """
+    for elem in i_list:
+    	if elem <= this:
+    		print("that")
+    	else:
+    		print(elem)
+
+# List comprehension version
+def if_this_not_that(i_list, this):
+	[print(i) if i > this else print("that") for i in i_list]
+
+
+
+
+"""
+>>> berkeley = make_city('Berkeley', 122, 37)
+>>> get_name(berkeley)
+'Berkeley'
+>>> get_lat(berkeley)
+122
+>>> new_york = make_city('New York City', 74, 40)
+>>> get_lon(new_york)
+40
+"""
+
+
+def distance(city1, city2):
+    """
+    >>> city1 = make_city('city1', 0, 1)
+    >>> city2 = make_city('city2', 0, 2)
+    >>> distance(city1, city2)
+    1.0
+    >>> city3 = make_city('city3', 6.5, 12)
+    >>> city4 = make_city('city4', 2.5, 15)
+    >>> distance(city3, city4)
+    5.0
+    """
+    lat1, lat2 = get_lat(city1), get_lat(city2)
+    lon1, lon2 = get_lon(city1), get_lon(city2)
+    return sqrt((lat1-lat2)**2 + (lon1-lon2)**2)
+
+
+
+def closer_city(lat, lon, city1, city2):
+    """
+    Returns the name of either city1 or city2, whichever is closest to
+    coordinate (lat, lon).
+
+    >>> berkeley = make_city('Berkeley', 37.87, 112.26)
+    >>> stanford = make_city('Stanford', 34.05, 118.25)
+    >>> closer_city(38.33, 121.44, berkeley, stanford)
+    'Stanford'
+    >>> bucharest = make_city('Bucharest', 44.43, 26.10)
+    >>> vienna = make_city('Vienna', 48.20, 16.37)
+    >>> closer_city(41.29, 174.78, bucharest, vienna)
+    'Bucharest'
+    """
+    city3 = make_city("arb", lat, lon)
+    distance1 = distance(city3, city2)
+    distance2 = distance(city3, city1)
+    if distance1 < distance2:
+    	return get_name(city2)
+    return get_name(city1)
+
+
+
+def flatten(lst):
+    """Returns a flattened version of lst.
+
+    >>> flatten([1, 2, 3])     # normal list
+    [1, 2, 3]
+    >>> x = [1, [2, 3], 4]      # deep list
+    >>> flatten(x)
+    [1, 2, 3, 4]
+    >>> x = [[1, [1, 1]], 1, [1, 1]] # deep list
+    >>> flatten(x)
+    [1, 1, 1, 1, 1, 1]
+    """
+    if not lst:
+    	return []
+    elif type(lst[0]) == list:
+    	return flatten(lst[0]) + flatten(lst[1:])
+    else:
+    	return [lst[0]] + flatten(lst[1:])
+
+
+
+def merge(lst1, lst2):
+    """Merges two sorted lists.
+
+    >>> merge([1, 3, 5], [2, 4, 6])
+    [1, 2, 3, 4, 5, 6]
+    >>> merge([], [2, 4, 6])
+    [2, 4, 6]
+    >>> merge([1, 2, 3], [])
+    [1, 2, 3]
+    >>> merge([5, 7], [2, 4, 6])
+    [2, 4, 5, 6, 7]
+    """
+    if not lst1 or not lst2:
+    	return lst1 + lst2
+    elif lst1[1] < lst2[1]:
+    	return [lst[1]] + merge(lst1[1:], lst2)
+    else:
+    	return [lst[2]] + merge(lst1, lst2[1:])
+
+
+
+def creat_row(size):
+	"""Returns a single, empty row with the given size. Each empty spot is
+    represented by the string '-'.
+
+    >>> create_row(5)
+    ['-', '-', '-', '-', '-']
+    """
+    return ['-' in i range(size)]
+
+
+def create_board(rows, columns):
+    """Returns a board with the given dimensions.
+
+    >>> create_board(3, 5)
+    [['-', '-', '-', '-', '-'], ['-', '-', '-', '-', '-'], ['-', '-', '-', '-', '-']]
+    """
+    return [create_row(columns) for i in range(rows)]
+
+
+
+def replace_elem(lst, index, elem):
+    """Create and return a new list whose elements are the same as those in
+    LST except at index INDEX, which should contain element ELEM instead.
+
+    >>> old = [1, 2, 3, 4, 5, 6, 7]
+    >>> new = replace_elem(old, 2, 8)
+    >>> new
+    [1, 2, 8, 4, 5, 6, 7]
+    >>> new is old   # check that replace_elem outputs a new list
+    False
+    """
+    assert index >= 0 and index < len(lst), 'Index is out of bounds'
+    return lst[:index] + [elem] + lst[index+1:]
+
+
+
+def get_piece(board, row, column):
+    """Returns the piece at location (row, column) in the board.
+
+    >>> rows, columns = 2, 2
+    >>> board = create_board(rows, columns)
+    >>> board = put_piece(board, rows, 0, 'X')[1] # Puts piece "X" in column 0 of board and updates board
+    >>> board = put_piece(board, rows, 0, 'O')[1] # Puts piece "O" in column 0 of board and updates board
+    >>> get_piece(board, 1, 0)
+    'X'
+    >>> get_piece(board, 1, 1)
+    '-'
+    """
+    return board[row][column]
+
+
+
+
+def put_piece(board, max_rows, column, player):
+    """Puts PLAYER's piece in the bottommost empty spot in the given column of
+    the board. Returns a tuple of two elements:
+
+        1. The index of the row the piece ends up in, or -1 if the column
+           is full.
+        2. The new board
+
+    >>> rows, columns = 2, 2
+    >>> board = create_board(rows, columns)
+    >>> row, new_board = put_piece(board, rows, 0, 'X')
+    >>> row
+    1
+    >>> row, new_board = put_piece(new_board, rows, 0, 'O')
+    >>> row
+    0
+    >>> row, new_board = put_piece(new_board, rows, 0, 'X')
+    >>> row
+    -1
+    """
+    curr_row_index = max_rows - 1
+    while curr_row_index >= 0 and get_piece(board, curr_row_index, column) != "-":
+        curr_row_index -= 1
+    if curr_row_index >= 0:
+        new_row = replace_elem(board[curr_row_index], column, player)
+        new_board = replace_elem(board, curr_row_index, new_row)
+        board = new_board
+    return curr_row_index, board
+
+
+
+def make_move(board, max_rows, max_cols, col, player):
+    """Put player's piece in column COL of the board, if it is a valid move.
+    Return a tuple of two values:
+
+        1. If the move is valid, make_move returns the index of the row the
+           piece is placed in. Otherwise, it returns -1.
+        2. The updated board
+
+    >>> rows, columns = 2, 2
+    >>> board = create_board(rows, columns)
+    >>> row, board = make_move(board, rows, columns, 0, 'X')
+    >>> row
+    1
+    >>> get_piece(board, 1, 0)
+    'X'
+    >>> row, board = make_move(board, rows, columns, 0, 'O')
+    >>> row
+    0
+    >>> row, board = make_move(board, rows, columns, 0, 'X')
+    >>> row
+    -1
+    >>> row, board = make_move(board, rows, columns, -4, '0')
+    >>> row
+    -1
+    """
+    if col < 0 or col >= max_cols:
+        return -1, board
+    return put_piece(board, max_rows, col, player)
+
+
+
+
+def print_board(board, max_rows, max_cols):
+    """Prints the board. Row 0 is at the top, and column 0 at the far left.
+
+    >>> rows, columns = 2, 2
+    >>> board = create_board(rows, columns)
+    >>> print_board(board, rows, columns)
+    - -
+    - -
+    >>> new_board = make_move(board, rows, columns, 0, 'X')[1]
+    >>> print_board(new_board, rows, columns)
+    - -
+    X -
+    """
+    for row in range(max_rows):
+        row_str = ''
+        for col in range(max_cols):
+            piece = get_piece(board, row, col)
+            row_str += piece + " "
+        print(row_str.strip())
+
+
+
+
+def check_win_row(board, max_rows, max_cols, num_connect, row, player):
+    """ Returns True if the given player has a horizontal win
+    in the given row, and otherwise False.
+
+    >>> rows, columns, num_connect = 4, 4, 2
+    >>> board = create_board(rows, columns)
+    >>> board = make_move(board, rows, columns, 0, 'X')[1]
+    >>> board = make_move(board, rows, columns, 0, 'O')[1]
+    >>> check_win_row(board, rows, columns, num_connect, 3, 'O')
+    False
+    >>> board = make_move(board, rows, columns, 2, 'X')[1]
+    >>> board = make_move(board, rows, columns, 0, 'O')[1]
+    >>> check_win_row(board, rows, columns, num_connect, 3, 'X')
+    False
+    >>> board = make_move(board, rows, columns, 1, 'X')[1]
+    >>> check_win_row(board, rows, columns, num_connect, 3, 'X')
+    True
+    >>> check_win_row(board, rows, columns, 4, 3, 'X')    # A win depends on the value of num_connect
+    False
+    >>> check_win_row(board, rows, columns, num_connect, 3, 'O')   # We only detect wins for the given player
+    False
+    """
+    adjacent = 0
+    for col in range(max_cols):
+        piece = get_piece(board, row, col)
+        if piece == player:
+            adjacent += 1
+        else:
+            adjacent = 0
+        if adjacent >= num_connect:
+            return True
+    return False
+
+
+
+
+def check_win_column(board, max_rows, max_cols, num_connect, col, player):
+    """ Returns True if the given player has a vertical win in the given column,
+    and otherwise False.
+
+    >>> rows, columns, num_connect = 5, 5, 2
+    >>> board = create_board(rows, columns)
+    >>> board = make_move(board, rows, columns, 0, 'X')[1]
+    >>> board = make_move(board, rows, columns, 1, 'O')[1]
+    >>> check_win_column(board, rows, columns, num_connect, 0, 'X')
+    False
+    >>> board = make_move(board, rows, columns, 1, 'X')[1]
+    >>> board = make_move(board, rows, columns, 1, 'O')[1]
+    >>> check_win_column(board, rows, columns, num_connect, 1, 'O')
+    False
+    >>> board = make_move(board, rows, columns, 2, 'X')[1]
+    >>> board = make_move(board, rows, columns, 1, 'O')[1]
+    >>> check_win_column(board, rows, columns, num_connect, 1, 'O')
+    True
+    >>> check_win_column(board, rows, columns, 4, 1, 'O')
+    False
+    >>> check_win_column(board, rows, columns, num_connect, 1, 'X')
+    False
+    """
+    adjacent = 0
+    for row in range(max_rows):
+        piece = get_piece(board, row, col)
+        if piece == player:
+            adjacent += 1
+        else:
+            adjacent = 0
+        if adjacent >= num_connect:
+            return True
+    return False
+
+
+
+
+
+def check_win(board, max_rows, max_cols, num_connect, row, col, player):
+    """Returns True if the given player has any kind of win passing through 
+    (row, col), and False otherwise.
+
+    >>> rows, columns, num_connect = 2, 2, 2
+    >>> board = create_board(rows, columns)
+    >>> board = make_move(board, rows, columns, 0, 'X')[1]
+    >>> board = make_move(board, rows, columns, 1, 'O')[1]
+    >>> board = make_move(board, rows, columns, 0, 'X')[1]
+    >>> check_win(board, rows, columns, num_connect, 0, 0, 'O')
+    False
+    >>> check_win(board, rows, columns, num_connect, 0, 0, 'X')
+    True
+
+    >>> board = create_board(rows, columns)
+    >>> board = make_move(board, rows, columns, 0, 'X')[1]
+    >>> board = make_move(board, rows, columns, 0, 'O')[1]
+    >>> board = make_move(board, rows, columns, 1, 'X')[1]
+    >>> check_win(board, rows, columns, num_connect, 1, 0, 'X')
+    True
+    >>> check_win(board, rows, columns, num_connect, 0, 0, 'X')
+    False
+
+    >>> board = create_board(rows, columns)
+    >>> board = make_move(board, rows, columns, 0, 'X')[1]
+    >>> board = make_move(board, rows, columns, 1, 'O')[1]
+    >>> board = make_move(board, rows, columns, 1, 'X')[1]
+    >>> check_win(board, rows, columns, num_connect, 0, 0, 'X')
+    False
+    >>> check_win(board, rows, columns, num_connect, 1, 0, 'X')
+    True
+    """
+    diagonal_win = check_win_diagonal(board, max_rows, max_cols, num_connect,
+                                      row, col, player)
+    return check_win_row(board, max_rows, max_cols, num_connect, row, player) or \
+             check_win_column(board, max_rows, max_cols, num_connect, col, player) or \
+               diagonal_win
